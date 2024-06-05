f431d3129ee2faa8e3bb5bd02db170c377eeaac0
JoaoClaudioSilva <143845972+JoaoClaudioSilva@users.noreply.github.com>
2024-06-05 19:45:29 -0300

    now sets the notes rated by user field in the database to null

--- src/components/auth/register.tsx
+++ src/components/auth/register.tsx
@@ -38,6 +38,7 @@ export default function Register() {
           email: email,
           role: role,
           password: password,
+          notesRated: null,
         });
         setEmail("");
         setPassword("");
